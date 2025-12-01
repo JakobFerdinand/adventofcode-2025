0decabb1b87a4f4e58251a325686d22fb1c3b821
Jakob Ferdinand Wegenschimmel <jakob.wegenschimmel@rzl.at>
2025-12-01 20:10:57 +0100

    day 1 - part 1

--- src/adventofcode/Program.cs
+++ src/adventofcode/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hello, Advent of Code!");
+
+var lines = File.ReadAllLines("input/input1.txt");
+
+int dail = 50;
+int password = 0;
+
+foreach (var line in lines)
+{
+	Console.WriteLine($"Current dail position: {dail}");
+	switch (line)
+	{
+		case string s when s[0] is 'L':
+			var x = int.Parse(s[1..]);
+			dail -= x % 100;
+			if (dail < 0)
+			{
+				dail += 100;
+			}
+			break;
+		case string s when s[0] is 'R':
+			var y = int.Parse(s[1..]);
+			dail += y % 100;
+			if (dail >= 100)
+			{
+				dail -= 100;
+			}
+			break;
+	}
+	if (dail is 0)
+	{
+		password++;
+	}
+}
+
+Console.WriteLine($"Password: {password}");
